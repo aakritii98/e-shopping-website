34f2466413d0439d44a16f682d158d400b6b35c6
aakritii98 <aakriti.ag98@gmail.com>
2021-06-06 14:04:55 +0530

    user product and display item added

---
+++ user/display_item.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class user_display_item : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=D:\shopping_website\App_Data\Shopping.mdf;Integrated Security=True;User Instance=True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "select * from product";
+        DataTable dt = new DataTable();
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(dt);
+        d1.DataSource = dt;
+        d1.DataBind();
+        con.Close();
+    }
+}
---
+++ user/product_desc.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class user_product_desc : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=D:\shopping_website\App_Data\Shopping.mdf;Integrated Security=True;User Instance=True");
+    int id;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString["id"] == null)
+        {
+            Response.Redirect("display_item.aspx");
+        }
+        else
+        {
+            id = Convert.ToInt32(Request.QueryString["id"].ToString());
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from product";
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            d1.DataSource = dt;
+            d1.DataBind();
+            con.Close();
+        }
+    }
+}
